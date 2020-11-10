f53072ba0d55e6fea7c89bf48e4c1a2b5cb2b477
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-11-10 16:43:35 -0500

    Added pins
    Pins act a little wonky though

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.maxAngularVelocity = 50f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
