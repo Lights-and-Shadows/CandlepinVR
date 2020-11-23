4d1e5e72f800688a24e2b02d52663ef6f30a2b82
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-11-23 12:33:16 -0500

    Adjustments

---
+++ Assets/Scripts/SpawnBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject returnArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ball")
+        {
+
+        }
+    }
+
+}
