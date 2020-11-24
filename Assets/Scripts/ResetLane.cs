3b3de484458f3d59b3fd57441b8329b49d308076
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-11-24 15:46:27 -0500

    Updates

---
+++ Assets/Scripts/ResetLane.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLane : MonoBehaviour
+{
+    public List<GameObject> pins, balls, pinSpawns, ballSpawns;
+
+    public void ResetTheLane()
+    {
+        foreach(GameObject spawn in pinSpawns)
+        {
+            pins[pinSpawns.IndexOf(spawn)].transform.position = spawn.transform.position;
+            pins[pinSpawns.IndexOf(spawn)].transform.rotation = spawn.transform.rotation;
+            pins[pinSpawns.IndexOf(spawn)].GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+        foreach(GameObject spawn in ballSpawns)
+        {
+            balls[ballSpawns.IndexOf(spawn)].transform.position = spawn.transform.position;
+            balls[ballSpawns.IndexOf(spawn)].transform.rotation = spawn.transform.rotation;
+            balls[ballSpawns.IndexOf(spawn)].GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
