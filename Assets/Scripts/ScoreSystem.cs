b989788bfaa37e1aacbc8033c3646ae3a5c96c6c
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2021-01-24 15:57:52 -0500

    Decision to remove additions, unsure how to proceed at this moment

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -3,28 +3,9 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 
-internal class ScoreSystem
+public class ScoreSystem : MonoBehaviour
 {
-    private Dictionary<Frame, int> markTracker;
-
-    public ScoreSystem(IEnumerable<Frame> frames)
-    {
-        markTracker = new Dictionary<Frame, int>();
-        foreach (var frame in frames)
-        {
-            markTracker.Add(frame, 0);
-        }
-    }
-
-    public void TrackRolls(Frame frame, int rollsToTrack)
-    {
-        markTracker[frame] = rollsToTrack;
-    }
-
-    public void HandleRoll(int score)
-    {
-
-    }
+    
 }
 
 
